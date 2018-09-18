2a8ba7a7675ae7a357baef95ced9ec4f48a14848
Marek Kisiel <leisikkeram@gmail.com>
2018-09-18 13:52:45 +0200

    Add github board service stump

---
+++ src/classes/KanbanBoard/Github/Board/GithubBoardInteractor.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marek.kisiel
+ * Date: 18/09/2018
+ * Time: 13:50
+ */
+
+namespace KanbanBoard\KanbanBoard\Github\Board;
+
+
+use KanbanBoard\KanbanBoard\Board\BoardInteractorInterface;
+
+class GithubBoardInteractor implements BoardInteractorInterface
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function getMilestones(): array
+    {
+        // TODO: Implement getMilestones() method.
+    }
+}
