f05b6464868e8b8f499c7e338e853bcc4038d7fa
Marek Kisiel <leisikkeram@gmail.com>
2018-09-20 11:43:51 +0200

    Implement new github client

--- src/classes/KanbanBoard/Github/Client/Github.php
+++ src/classes/KanbanBoard/Github/Client/Github.php
@@ -8,24 +8,115 @@
 
 namespace KanbanBoard\Github\Client;
 
+use Cache\Adapter\Filesystem\FilesystemCachePool;
+use Github\Client;
 use KanbanBoard\Client\ClientInterface;
+use KanbanBoard\Config\ConfigInterface;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem;
 
 class Github implements ClientInterface
 {
 
     /**
-     * @return array
+     * @var \KanbanBoard\Config\ConfigInterface
      */
-    public function getMilestones(): array
+    private $config;
+
+    /**
+     * @var \Github\Client
+     */
+    private $githubClient;
+
+    /**
+     * Milestones per repository.
+     *
+     * @var array
+     */
+    private $milestones;
+
+    /**
+     * Issues per milestone id.
+     *
+     * @var array
+     */
+    private $issues;
+
+    /**
+     * Initialize Github API.
+     *
+     * @param \KanbanBoard\Config\ConfigInterface $config
+     */
+    public function __construct(ConfigInterface $config)
     {
-        // TODO: Implement getMilestones() method.
+        $this->config = $config;
+        //@TODO Consider Client also injectable.
+        $this->githubClient = new Client();
+
+        $this->githubClient->authenticate(
+            $config->getClientId(),
+            $config->getClientSecret(),
+            Client::AUTH_URL_CLIENT_ID
+        );
+
+        //@TODO Consider directory path configurable also.
+        $filesystemAdapter = new Local(__DIR__ . '../../../../../.cache');
+        $filesystem = new Filesystem($filesystemAdapter);
+        // Adding cache layer can be also done in wrapper class (decorator).
+        $pool = new FilesystemCachePool($filesystem);
+        $this->githubClient->addCache($pool);
     }
 
     /**
      * @return array
+     *    Milestones for every available repository, keyed by repository name.
+     * @throws \Exception
+     *    When repository names in config file are not available.
+     */
+    public function getMilestones(): array
+    {
+        if (isset($this->milestones)) {
+            return $this->milestones;
+        }
+
+        $repositories = $this->config->getRepositoryList();
+
+        if (empty($repositories)) {
+            throw new \Exception("There aren't any repository configured from to fetch issues");
+        }
+
+        foreach ($repositories as $repository) {
+            $milestones = $this->githubClient->api('issues')
+              ->milestones($this->config->getAccountName(), $repository);
+
+            $this->milestones[$repository] = is_array($milestones) ? $milestones: [];
+        }
+
+        return $this->milestones;
+    }
+
+    /**
+     * @return array
+     *    Array of issues keyed by milestone ID.
+     * @throws \Exception
      */
     public function getIssues(): array
     {
-        // TODO: Implement getIssues() method.
+        if (isset($this->issues)) {
+            return $this->issues;
+        }
+
+        $milestones = $this->getMilestones();
+        $this->issues = [];
+
+        foreach ($milestones as $repository => $milestone) {
+            $id = $milestone['number'];
+            $params = ['milestone' => $id, 'state' => 'all'];
+            $issues = $this->githubClient->api('issues')
+              ->all($this->config->getAccountName(), $repository, $params);
+            $this->issues[$id] = is_array($issues) ? $issues : [];
+        }
+
+        return $this->issues;
     }
 }
