0f809ecd6fa380b0325aa824d0f1e239bfb3123d
Marek Kisiel <leisikkeram@gmail.com>
2018-09-21 14:30:41 +0200

    Add basic github board interactor implementation

--- src/classes/KanbanBoard/Github/Board/BoardInteractor.php
+++ src/classes/KanbanBoard/Github/Board/BoardInteractor.php
@@ -9,15 +9,58 @@
 namespace KanbanBoard\Github\Board;
 
 use KanbanBoard\Board\BoardInteractorInterface;
+use KanbanBoard\Client\ClientInterface;
+use KanbanBoard\Github\Entity\Issue;
+use KanbanBoard\KanbanBoard\Github\Entity\Milestone;
 
+/**
+ * The purpose of this class is to map API data into entites.
+ * More like Mapper, maybe "Interactor" is not best name for this.
+ *
+ * @package KanbanBoard\Github\Board
+ */
 class BoardInteractor implements BoardInteractorInterface
 {
 
     /**
-     * @inheritdoc
+     * @var \KanbanBoard\Client\ClientInterface
+     */
+    private $client;
+
+    /**
+     * @param \KanbanBoard\Client\ClientInterface $client
+     */
+    public function __construct(ClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * {@inheritdoc}
      */
     public function getMilestones(): array
     {
-        // TODO: Implement getMilestones() method.
+        $milestones = [];
+        $milestonesData = $this->client->getMilestones();
+        $issues = $this->client->getIssues();
+
+        foreach ($milestonesData as $repository => $repositoryMilestones) {
+            foreach ($repositoryMilestones as $milestoneData) {
+                $id = $milestoneData['id'];
+                $milestone = new Milestone($id);
+                $milestone->setTitle($milestoneData['title']);
+
+                if (isset($issues[$id]) && !empty($issues[$id])) {
+                    foreach ($issues[$id] as $issueData) {
+                        $issue = new Issue($issueData['id']);
+                        $issue->setTitle($issueData['title']);
+                        $milestone->addIssue($issue);
+                    }
+                }
+
+                $milestones[] = $milestone;
+            }
+        }
+        return $milestones;
     }
 }
